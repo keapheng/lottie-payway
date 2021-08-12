85b8b51c83f5d0c38b218afc52101916ca817d81
CHHUM Sina <chhum.sina@ababank.com>
2021-08-12 11:50:52 +0700

    asdfsd

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,29 @@
 <template>
-  <Tutorial/>
+            <lottie :options="defaultOptions" :height="600" :width="600" v-on:animCreated="handleAnimation"/>
 </template>
 
 <script>
-export default {}
+import Lottie from 'vue-lottie';
+import * as animationData from '@/assets/image.json';
+export default {
+    name: "HomepageHeader",
+    components: {
+      'lottie': Lottie
+    },
+    data() {
+        return {
+            defaultOptions: {animationData: animationData.default},
+            animationSpeed: 1,
+            anim: null
+        }
+    },
+    methods: {
+      handleAnimation: function (anim) {
+        this.anim = anim;
+      }
+    }
+}
 </script>
+
+<style scoped>
+</style>
